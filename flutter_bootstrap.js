9f5ee28f078cbb98c356f5c4f3a160f3dbe680d3
Sheikh Ahmadullah <sheikh@Sheikhs-Mac-mini.local>
2025-12-13 12:59:10 +0530

    ui update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3529303530"
+    serviceWorkerVersion: "4019189713"
   }
 });
